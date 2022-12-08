6700460d22fabd3181e487a23b715b01c7f3820b
admin <ramakanth.ileni@gmail.com>
2022-12-08 21:15:49 +1100

    IPyspark code added

--- com/ingrity/batch/2022/bigdata/PracticePySpark.py
+++ com/ingrity/batch/2022/bigdata/PracticePySpark.py
@@ -1,1 +1,24 @@
 import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+
+if __name__ == "__main__":
+    spark = SparkSession.Builder().appName("Practice").master("local[4]").getOrCreate()
+
+    df = spark.read.csv("D:\Support\snowflakedata\PersonDemographics\PersonDemographics2.csv", header=True)
+    #df.groupby("Gender").agg("TotalChildren").show(100, False)
+    df.createTempView("tbl")
+    spark.sql("select sum(TotalChildren), gender from tbl group by gender ").show(100)
+
+    df.groupby("Gender").count().show(100, False)
+    df.filter(col("gender") == 'M').show()
+    df_m = spark.sql("select BusinessEntityID, YearlyIncome, Education from tbl where gender = 'M'")
+
+    df_m.write.parquet("D:\Support\snowflakedata\out3")
+
+    #df_m.write.jdbc()
+
+
+
+    #df.show(1000, False)
